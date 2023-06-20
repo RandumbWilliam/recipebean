077be7fb97a28cbaba69f829c8f3f0f7a9b6ee29
William Zhen <william.zhen8166@hotmail.com>
2023-06-20 13:27:52 -0400

    fix: environment postgres

--- server/src/orm.config.ts
+++ server/src/orm.config.ts
@@ -1,4 +1,5 @@
 import { MikroORM } from "@mikro-orm/core";
+import "dotenv-safe/config";
 
 export default {
   migrations: {
@@ -7,11 +8,11 @@ export default {
     transactional: true,
   },
   tsNode: process.env.NODE_DEV === "true" ? true : false,
-  user: "postgres",
-  password: "2701",
-  dbName: "product",
-  host: "localhost",
-  port: 5432,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  dbName: process.env.POSTGRES_DB_NAME,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
   entities: ["dist/**/*.entity.js"],
   entitiesTs: ["src/**/*.entity.ts"],
   type: "postgresql",
