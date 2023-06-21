8ae643c8e3433a7b72e3b4278179bf6dfa0e293f
William Zhen <william.zhen8166@hotmail.com>
2023-06-20 20:01:38 -0400

    cookie urql client

--- client/src/utils/createUrqlClient.ts
+++ client/src/utils/createUrqlClient.ts
@@ -13,74 +13,83 @@ import { betterUpdateQuery } from "./betterUpdateQuery";
 const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/graphql";
 
-export const createUrqlClient = (ssrExchange: any) => ({
-  url: API_URL,
-  fetchOptions: {
-    credentials: "include" as const,
-  },
-  exchanges: [
-    dedupExchange,
-    cacheExchange({
-      updates: {
-        Mutation: {
-          deleteUser: (_result, args, cache, info) => {
-            betterUpdateQuery<DeleteUserMutation, MyUserQuery>(
-              cache,
-              { query: MyUserDocument },
-              _result,
-              (result, query) => {
-                if (result.deleteUser.errors || !result.deleteUser.boolean) {
-                  return query;
-                } else {
-                  return { myUser: null };
+const isServer = () => typeof window === "undefined";
+
+export const createUrqlClient = (ssrExchange: any, ctx: any) => {
+  let cookie = "";
+  if (isServer()) {
+    cookie = ctx?.req?.headers?.cookie;
+  }
+
+  return {
+    url: API_URL,
+    fetchOptions: {
+      credentials: "include" as const,
+    },
+    exchanges: [
+      dedupExchange,
+      cacheExchange({
+        updates: {
+          Mutation: {
+            deleteUser: (_result, args, cache, info) => {
+              betterUpdateQuery<DeleteUserMutation, MyUserQuery>(
+                cache,
+                { query: MyUserDocument },
+                _result,
+                (result, query) => {
+                  if (result.deleteUser.errors || !result.deleteUser.boolean) {
+                    return query;
+                  } else {
+                    return { myUser: null };
+                  }
                 }
-              }
-            );
-          },
-          logout: (_result, args, cache, info) => {
-            betterUpdateQuery<LogoutMutation, MyUserQuery>(
-              cache,
-              { query: MyUserDocument },
-              _result,
-              () => ({ myUser: null })
-            );
-          },
-          login: (_result, args, cache, info) => {
-            betterUpdateQuery<LoginMutation, MyUserQuery>(
-              cache,
-              { query: MyUserDocument },
-              _result,
-              (result, query) => {
-                if (result.login.errors) {
-                  return query;
-                } else {
-                  return {
-                    myUser: result.login.user,
-                  };
+              );
+            },
+            logout: (_result, args, cache, info) => {
+              betterUpdateQuery<LogoutMutation, MyUserQuery>(
+                cache,
+                { query: MyUserDocument },
+                _result,
+                () => ({ myUser: null })
+              );
+            },
+            login: (_result, args, cache, info) => {
+              betterUpdateQuery<LoginMutation, MyUserQuery>(
+                cache,
+                { query: MyUserDocument },
+                _result,
+                (result, query) => {
+                  if (result.login.errors) {
+                    return query;
+                  } else {
+                    return {
+                      myUser: result.login.user,
+                    };
+                  }
                 }
-              }
-            );
-          },
-          register: (_result, args, cache, info) => {
-            betterUpdateQuery<RegisterMutation, MyUserQuery>(
-              cache,
-              { query: MyUserDocument },
-              _result,
-              (result, query) => {
-                if (result.register.errors) {
-                  return query;
-                } else {
-                  return {
-                    myUser: result.register.user,
-                  };
+              );
+            },
+            register: (_result, args, cache, info) => {
+              betterUpdateQuery<RegisterMutation, MyUserQuery>(
+                cache,
+                { query: MyUserDocument },
+                _result,
+                (result, query) => {
+                  if (result.register.errors) {
+                    return query;
+                  } else {
+                    return {
+                      myUser: result.register.user,
+                    };
+                  }
                 }
-              }
-            );
+              );
+            },
           },
         },
-      },
-    }),
-    ssrExchange,
-    fetchExchange,
-  ],
-});
+      }),
+      ssrExchange,
+      fetchExchange,
+    ],
+  };
+};
