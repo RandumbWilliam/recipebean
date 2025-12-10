7d0d23c5afd77df467e4596376d54aad2aa55813
William <william@Williams-MacBook-Air.local>
2025-12-09 23:49:24 -0500

    chore: migrate console

--- migrate.js
+++ migrate.js
@@ -3,6 +3,8 @@ import { drizzle } from 'drizzle-orm/node-postgres'
 import { migrate } from 'drizzle-orm/node-postgres/migrator'
 import 'dotenv/config'
 
+console.log(process.env.DATABASE_URL)
+
 const db = drizzle(process.env.DATABASE_URL)
 
 migrate(db, { migrationsFolder: './server/db/migrations' })
